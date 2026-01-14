ff0b21e36dd8b6bc03c022e04c2464a20f818a63
Silas Teague <silasteague@gmail.com>
2026-01-14 17:38:00 -0500

    Move sprite to next frame on watering

--- garden.js
+++ garden.js
@@ -5,16 +5,25 @@ for (let i = 0; i < tiles.length; i++) {
     let tile = tiles[i];
 
     // Add image to tile div
+    let imageContainer = document.createElement('div');
+    imageContainer.className = "plant-container";
+    tile.appendChild(imageContainer);
     let imageElement = document.createElement('img');
-    imageElement.src = "sprites/plant1/stage1.png";
+    imageElement.src = "sprites/plant2/Monstera.png";
     imageElement.className = "plant";
-    tile.appendChild(imageElement);
+    imageElement.id = `plant-${i}`;
+    imageContainer.appendChild(imageElement);
 
     // Add watering button
     let wateringButton = document.createElement('button');
     wateringButton.className = 'watering-button';
     wateringButton.id = `watering-button${i + 1}`;
     wateringButton.innerHTML = 'Water!';
+    wateringButton.addEventListener('click', () => {
+        let plant = document.querySelector(`#plant-${i}`);
+        let plantWidth = plant.clientHeight;
+        plant.style.transform = `translate(-${plantWidth}px, 0)`;
+    })
     tile.appendChild(wateringButton);
 
     console.log(`Loaded tile ${i + 1}`);
